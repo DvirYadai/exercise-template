7dce1c98e87e63a41b69e37a1716e8d758d91b98
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 22:06:28 +0200

    q24 answer

--- q24.js
+++ q24.js
@@ -3,9 +3,14 @@ function run() {
     const output = document.getElementById("output");
     let out = "";
 /**/
-    // write your code here
-    // out = input + 5;
-
+    const arr = eval(input);
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== null && arr[i] !== 0 && arr[i] !== "\"\"" && arr[i] !== false && arr[i] !== undefined && !(Number.isNaN(arr[i]))){
+            newArr.push(arr[i]);
+        }
+    }
+    out = newArr;
 /**/
     output.innerText = out;
 }
