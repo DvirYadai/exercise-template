9b3b94b991b4b20b29892940204e0378b4b0b52a
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 20:21:06 +0200

    Q20 answer

--- q20.js
+++ q20.js
@@ -3,9 +3,30 @@ function run() {
     const output = document.getElementById("output");
     let out = "";
 /**/
-    // write your code here
-    // out = input + 5;
-
+    let arr = JSON.parse(input);
+    let arrDuplicate = [];
+    let count = 0;
+    let num;
+    for(let i = 0; i < arr.length; i++){
+        count = 0;
+        num = arr[i];
+        for(let j = 0; j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                count++;
+            }
+        }
+        if(count > 1){
+            let isBoo = false;
+            for(let z = 0; z <arrDuplicate.length; z++){
+                if(num === arrDuplicate[z]){
+                    isBoo = true;
+                }
+            }
+            if(!isBoo)
+                arrDuplicate.push(arr[i]);
+        }
+    }
+    out = arrDuplicate;
 /**/
     output.innerText = out;
 }
