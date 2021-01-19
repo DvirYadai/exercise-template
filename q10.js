c8ddfaefeec187bacc8e1371c1c478b30960b60f
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 15:38:54 +0200

    Q1 answer

--- q10.js
+++ q10.js
@@ -2,7 +2,10 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-    const arr = JSON.parse(input);
-    out = ({}).toString.call(arr) === '[object Array]' ? 'true' : 'false';
+/**/
+    // write your code here
+    // out = input + 5;
+
+/**/
     output.innerText = out;
 }
