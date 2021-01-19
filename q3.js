a6674df97b0cd086b5bd04a492f05a7c4c362e0c
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 16:07:16 +0200

    Q3 answer

--- q3.js
+++ q3.js
@@ -1,11 +1,20 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input2 = document.getElementById("input2").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
+    const newArr = [];
+    const arr = JSON.parse(input);
+    if(input2 === ''){
+        out = arr[0];
+    } else if (input2 < 0){
+        out = '[]';
+    } else {
+        for(let i = 0; i < input2; i++){
+            newArr.push(arr[i]);
+        }
+        out = newArr;
+    }
     output.innerText = out;
 }
