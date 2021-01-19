539609c8ce0428aef877ffea1ab2853600d9996f
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 18:41:08 +0200

    Q13 answer

--- q13.js
+++ q13.js
@@ -1,11 +1,13 @@
-function run() {
-    const input = document.getElementById("input").value;
+let arr = [];
+let i = 1;
+function display() {
     const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    let out = [];
+    out = arr;
     output.innerText = out;
+}
+
+function add(){
+    arr.push('Element ' + i + ' = ' + document.getElementById("input").value + '\n');
+    i++;
 }
