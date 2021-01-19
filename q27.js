a41c226fb0a894e5ca0c569d051ed518fc24dded
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-19 22:32:02 +0200

    Q27 answer

--- q27.js
+++ q27.js
@@ -1,11 +1,17 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input2 = document.getElementById("input2").value;
     const output = document.getElementById("output");
     let out = "";
 /**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    const arr = eval(input);
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].hasOwnProperty(input2)){
+            newArr.push(arr[i][input2]);
+        }
+    }
+    out = newArr;
+    /**/
     output.innerText = out;
 }
